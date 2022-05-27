9d785d056294d453aeb58bf0cbd02270ebc16575
Justin Newton <justin-newton@outlook.com>
2022-05-27 08:04:28 -0500

    Week 2 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,7 +1,5 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
 const Campsite = require("../models/campsite");
-
 const campsiteRouter = express.Router();
 
 campsiteRouter
